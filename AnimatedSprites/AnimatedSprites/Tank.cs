e0c9e0c8fa2f3611175af1ba38319a7d97d49c69
Pro0f <makeyev.alexandr@gmail.com>
2014-03-04 21:21:29 +0200

    beginning task #2

--- AnimatedSprites/AnimatedSprites/Tank.cs
+++ AnimatedSprites/AnimatedSprites/Tank.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 
 namespace AnimatedSprites
 {
@@ -10,6 +12,7 @@ namespace AnimatedSprites
     abstract class Tank : Sprite
     {
         GameSettings.SpriteSettings MissileSetting { get; set; }
+        Missile currentMissle { get; set; }
 
         public Tank(GameSettings.SpriteSettings tankSettings, GameSettings.SpriteSettings missileSetting)
             : base(tankSettings)
@@ -20,6 +23,24 @@ namespace AnimatedSprites
         public Missile Fire()
         {
             return new Missile(MissileSetting, MoveDirection);
+        }
+
+        public Vector2 GetMissileStartPosition(Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.Up:
+                    
+                    break;
+                case Direction.Left:
+                    break;
+                case Direction.Right:
+                    break;
+                case Direction.Down:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
