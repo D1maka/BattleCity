dac33e420b358ae7a86727e0ce39609118d5228f
Pro0f <makeyev.alexandr@gmail.com>
2014-03-04 23:11:41 +0200

    GetMissileStartPosition()
    В файлі Sprite створити enum SpriteState {Alive,Destoyed}
    В класі Sprite створити поле SpriteState State.
    в класі Tank замість конструкції currentMissile!=null i
    currentMissile.IsOutOfBound == false використовувати
    currentMissile!=null i currentMissile.State == SpriteState.Alive

--- AnimatedSprites/AnimatedSprites/Tank.cs
+++ AnimatedSprites/AnimatedSprites/Tank.cs
@@ -22,25 +22,45 @@ namespace AnimatedSprites
 
         public Missile Fire()
         {
-            return new Missile(MissileSetting, MoveDirection);
+            if (currentMissle != null && currentMissle.State == SpriteState.Alive)
+            {
+                MissileSetting.StartPosition = GetMissileStartPosition();
+                currentMissle = new Missile(MissileSetting, MoveDirection);
+            }
+
+            return currentMissle;
         }
 
-        public Vector2 GetMissileStartPosition(Direction dir)
+        public Vector2 GetMissileStartPosition()
         {
+            Direction dir = DrawDirection;
+            Vector2 missileStartPosition = new Vector2();
+
             switch (dir)
             {
                 case Direction.Up:
-                    
+                    missileStartPosition.X = position.X + Settings.FrameSize.X / 2 - MissileSetting.FrameSize.X / 2;
+                    missileStartPosition.Y = position.Y - MissileSetting.FrameSize.Y;
                     break;
                 case Direction.Left:
+                    missileStartPosition.X = position.X - MissileSetting.FrameSize.X;
+                    missileStartPosition.Y = position.Y + Settings.FrameSize.Y / 2 - MissileSetting.FrameSize.Y / 2;
                     break;
                 case Direction.Right:
+                    missileStartPosition.X = position.X + Settings.FrameSize.X;
+                    missileStartPosition.Y = position.Y + Settings.FrameSize.Y / 2 - MissileSetting.FrameSize.Y / 2;
                     break;
                 case Direction.Down:
+                    missileStartPosition.X = position.X + Settings.FrameSize.X / 2 - MissileSetting.FrameSize.X / 2;
+                    missileStartPosition.Y = position.Y + Settings.FrameSize.Y;
+                    break;
+                case Direction.None:
                     break;
                 default:
                     break;
             }
+
+            return missileStartPosition;
         }
     }
 }
